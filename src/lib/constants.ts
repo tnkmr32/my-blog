478170c472e372beec5413b127589fe08e467e90
t-nakamura <tnakkrkr@gmail.com>
2025-09-09 08:21:50 +0900

    GitHubリンク修正

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const GITHUB_REPO_URL = "https://github.com/tnkmr32/my-blog";
+export const GITHUB_REPO_URL = "https://github.com/tnkmr32";
 export const CMS_NAME = "Markdown";
 export const SITE_NAME = "Toru Nakamura";
 export const HOME_OG_IMAGE_URL = "/assets/posts/og/og-image.png";
