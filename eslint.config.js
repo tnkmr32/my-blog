ae0bd0ca5f779200591f1757c684b0e2242f50b3
t-nakamura <tnakkrkr@gmail.com>
2025-10-12 12:38:06 +0900

    eslint設定作成

--- eslint.config.js
+++ eslint.config.js
@@ -1,88 +1,71 @@
-import eslint from "@eslint/js";
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import typescriptParser from "@typescript-eslint/parser";
-// import importPlugin from "eslint-plugin-import";
-import react from "eslint-plugin-react";
-import reactHooks from "eslint-plugin-react-hooks";
-// import unusedImports from "eslint-plugin-unused-imports";
-import globals from "globals";
-import prettierConfig from "eslint-config-prettier";
+import eslint from '@eslint/js';
+import {defineConfig, globalIgnores} from 'eslint/config';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import react from 'eslint-plugin-react';
+import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 
-export default [
+export default defineConfig([
   eslint.configs.recommended,
-  
+  // Ignore files and directories
+  globalIgnores([
+    'node_modules/**',
+    '.next/**',
+    '.vscode/**',
+    'next-env.d.ts',
+    '.prettierrc.cjs',
+    'eslint.config.js',
+  ]),
+  // TypeScript specific rules
+  // https://github.com/typescript-eslint/typescript-eslint/blob/main/packages/eslint-plugin/src/configs/eslintrc/recommended-type-checked.ts
+  tseslint.configs.recommendedTypeChecked,
   {
-    ignores: [".next/**", ".vscode/**", "next-env.d.ts"],
-  },
-  // base config
-  {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      ts: tseslint.plugin,
+    },
     languageOptions: {
-      globals: {
-        ...globals.browser,
-        ...globals.node,
-        ...globals.es5,
-      },
-      parser: typescriptParser,
       parserOptions: {
-        ecmaVersion: "latest",
-        sourceType: "module",
+        project: './tsconfig.json', // 型解析のためのTypeScript設定ファイルを指定
+      },
+    },
+    rules: {
+      // https://typescript-eslint.io/rules/no-deprecated/
+      'ts/no-deprecated': 'error',
+    },
+  },
+  // React specific rules
+  // https://www.npmjs.com/package/eslint-plugin-react
+  // https://www.npmjs.com/package/eslint-plugin-react-hooks
+  {
+    files: ['**/*.{ts,tsx}'],
+    plugins: {
+      react,
+      reactHooks,
+    },
+    languageOptions: {
+      parserOptions: {
         ecmaFeatures: {
           jsx: true,
         },
       },
-      plugins: {
-        "@typescript-eslint": typescriptEslint,
+      globals: {
+        ...globals.browser,
       },
     },
-  },
-  // React config
-  {
-    files: ["**/*.{ts,tsx}"],
-    plugins: {
-      react: react,
-      "react-hooks": reactHooks,
-    },
     rules: {
-      // https://typescript-eslint.io/rules/no-deprecated/
-      // "@typescript-eslint/no-deprecated": "error",
+      // TODO：ルール設定の精査
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
     },
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
-  // TSX specific rules
-  {
-    files: ["**/*.tsx"],
-    rules: {
-      "@typescript-eslint/no-misused-promises": [
-        "error",
-        {
-          checksVoidReturn: false,
-        },
-      ],
-    },
-  },
-  // Test files configuration
-  {
-    files: ["**/*.test.{ts,tsx}"],
-    plugins: {
-      jest: jest,
-      "@typescript-eslint": typescriptEslint,
-    },
-    rules: {
-      // Jest recommended rules
-      ...jest.configs.recommended.rules,
-      // Override for test files
-      "@typescript-eslint/unbound-method": "off",
-      "jest/unbound-method": "error",
-    },
-  },
-  // https://www.npmjs.com/package/eslint-plugin-react-hooks
-  reactHooks.configs['recommended-latest'],
   // eslint-config-prettier を最後に適用する
-  prettierConfig,
-];
+  // prettierと競合する可能性のあるルールをすべて無効化する
+  eslintConfigPrettier,
+]);
